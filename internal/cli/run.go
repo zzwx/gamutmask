525604e9e1501b223168d4efc345d1c141c3c142
zzwx <8169082+zzwx@users.noreply.github.com>
2017-12-23 08:07:44 -0500

    Import fix

--- internal/cli/run.go
+++ internal/cli/run.go
@@ -10,8 +10,9 @@ import (
 
 	"strconv"
 
+	"gopkg.in/cheggaaa/pb.v1"
+
 	"github.com/zzwx/gamutmask/internal/lib"
-	"github.com/zzwx/gamutmask/vendor_/gopkg.in/cheggaaa/pb.v1"
 )
 
 // Run will execute GenerateGamutMask against inputFileName and generate outputFileName
